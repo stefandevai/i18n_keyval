b250b398e8bff7e88fae0c3438bd192a7367c0cb
Stefan Devai <stefandevai@posteo.me>
2024-02-03 17:39:01 +0100

    Fix file portability issues

--- src/i18n/util/file.cpp
+++ src/i18n/util/file.cpp
@@ -1,23 +1,26 @@
 #include "i18n/util/file.hpp"
 
+#include <cstring>
+
 namespace i18n::util
 {
 std::string read_file(const std::filesystem::path filepath)
 {
-  FILE* file = fopen(filepath.c_str(), "rt");
+  const auto filepath_str = filepath.string();
+  FILE* file = std::fopen(filepath_str.c_str(), "rt");
 
   if (file == nullptr)
   {
     return "";
   }
 
-  fseek(file, 0, SEEK_END);
+  std::fseek(file, 0, SEEK_END);
   unsigned long length = ftell(file);
   char* data = new char[length + 1];
-  memset(data, 0, length + 1);
-  fseek(file, 0, SEEK_SET);
-  fread(data, 1, length, file);
-  fclose(file);
+  std::memset(data, 0, length + 1);
+  std::fseek(file, 0, SEEK_SET);
+  std::fread(data, 1, length, file);
+  std::fclose(file);
 
   std::string result(data);
   delete[] data;
